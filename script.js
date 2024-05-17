c167785a637becbee4946d4eca1f8e483d28138d
SlavikBadBoy <buracenkoslavik@gmail.com>
2024-05-17 20:26:26 +0300

    правки з JS

--- script.js
+++ script.js
@@ -28,13 +28,13 @@ for (let i = 0; i < fiveStrings.length; i++) {
   }
 }
 // Створити масив з 10-ти чисел. Знайти та вивести на екран максимальне значення з масиву.
-let tenNumbers = [1, 22, 3, 44, 5, 66, 7, 88, 9, 100]; // Створюємо масив з 10-ти чисел
-let maxNumbers = tenNumbers[0]; // Ініціалізуємо змінну для максимального значення першим елементом масиву
+let tenNumbers = [1, 22, 3, 44, 5, 66, 7, 88, 9, 100]; 
+let maxNumbers = tenNumbers[0]; 
 for (let i = 1; i <= tenNumbers.length; i += tenNumbers) {
-  // Виконуємо цикл, починаючи з другого елемента
+
   if (tenNumbers[i] > maxNumbers) {
-    // Перевіряємо, чи поточний елемент більший за max
-    maxNumbers = tenNumbers[i]; // Якщо так, оновлюємо max
+  
+    maxNumbers = tenNumbers[i]; 
   }
 }
 console.log(maxNumbers); 
