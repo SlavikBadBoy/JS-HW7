da703bd80b72d7346ec8201a51eca146a8ec5dfd
SlavikBadBoy <buracenkoslaviK@gmail.com>
2024-07-20 23:19:26 +0300

    7

--- script.js
+++ script.js
@@ -3,43 +3,57 @@
 const numbers = [1, 2, 3];
 numbers[1] = 10;
 console.log(numbers);
-// Створив масив із трьох рядків. Додати до масиву ще одну рядків.
-const strings = ["Один", "два", "три"];
-strings.push("чотири");
-console.log(strings);
+// Масив з трьох рядків
+const stringsArray = ["Hello", "World", "!"];
+
+stringsArray.push("JavaScript");
+
+console.log(stringsArray);
 // Створив скрипт який поверне суму всіх чисел в масиві.
 let sumNumbers = [1, 2, 3, 4, 5];
-let valueSum = 0;
-for (let i = 0; i <= sumNumbers.length; i += sumNumbers) {
-  valueSum += sumNumbers[i];
+let sum = 0;
+for (const number of numbers) {
+  sum += number;
 }
-console.log(valueSum);
+console.log(sum);
 
 // Створити масив з 5-ти чисел. Вивести на екран всі елементи масиву за допомогою циклу for.
-const fiveNumbers = [1, 2, 3, 4, 5];
-for (let i = 0; i <= fiveNumbers.length; i += fiveNumbers) {
-  console.log(fiveNumbers[i]);
+const numbersArray = [2, 4, 6, 8, 10];
+
+// Цикл for для виведення елементів
+for (let i = 0; i < numbersArray.length; i++) {
+  console.log(numbersArray[i]);
 }
+
 // Створити масив із 5-ти рядків. Вивести на екран кожен рядок з масиву, який містить більше 5-ти символів.
-const fiveStrings = ["Один", "два", "три", "чотири", "п'ять"];
-for (let i = 0; i <= fiveStrings.length; i+= fiveStrings) {
-  if (fiveStrings[i].length > 5) {
-    console.log(fiveStrings[i]);
+const stringsJsArray = ["Hello", "JavaScript", "World", "!", "Programming"];
+
+for (const string of stringsJsArray) {
+  if (string.length > 5) {
+    console.log(string);
   }
 }
 // Створити масив з 10-ти чисел. Знайти та вивести на екран максимальне значення з масиву.
 let tenNumbers = [1, 22, 3, 44, 5, 66, 7, 88, 9, 100];
-let maxNumbers = tenNumbers[0];
-for (let i = 1; i <= tenNumbers.length; i += tenNumbers) {
-  if (tenNumbers[i] > maxNumbers) {
-    maxNumbers = tenNumbers[i];
+const findMax = (numbers) => {
+  let max = numbers[0];
+  for (const number of numbers) {
+    if (number > max) {
+      max = number;
+    }
   }
-}
-console.log(maxNumbers);
+  return max;
+};
+
+const maxValue = findMax(tenNumbers);
+console.log(maxValue);
 
 // Створити масив з 10-ти чисел. Знайти всі парні числа в масиві та вивести їх на екран.
-for (let i = 0; i <= tenNumbers.length; i += tenNumbers) {
-  if (tenNumbers[i] % 2 === 0) {
-    console.log(tenNumbers[i]);
+const pairNumbersArray = [1, 5, 7, 3, 9, 2, 4, 6, 8, 10];
+
+// Цикл for для пошуку парних чисел
+for (const number of pairNumbersArray) {
+  if (number % 2 === 0) {
+    console.log(number);
   }
 }
